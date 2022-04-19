0386aa18543409b6aed22afd1b95edba95da4e29
vsbdn <v.boldurean@gmail.com>
2022-04-19 08:29:14 +0100

    Initial commit, add project structure & deps

---
+++ Makefile
@@ -0,0 +1,16 @@
+setup:
+	npm ci
+	npx simple-git-hooks
+
+dev:
+	npm run dev
+
+lint:
+
+	npm run lint
+
+build:
+	npm run build
+
+start:
+	npm run start
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
