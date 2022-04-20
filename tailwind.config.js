a1c6e6eb71b129f8cff9c30f16b25c291ff5fbd9
serg-by <sergmolby@gmail.com>
2022-04-20 11:27:29 +0300

    creat your color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./components/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // creat your color palette (orange - primary ?500 ? 400)
+        'osob': {
+          '50': '#fff7f2', 
+          '100': '#ffeee6', 
+          '200': '#ffd6bf', 
+          '300': '#ffbd99', 
+          '400': '#ff8b4d', 
+          '500': '#ff5900', 
+          '600': '#e65000', 
+          '700': '#bf4300', 
+          '800': '#993500', 
+          '900': '#7d2c00'
+      }
+      }
+    },
   },
   plugins: [],
 }
